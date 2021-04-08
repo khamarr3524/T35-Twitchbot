ff1a9f5300f09146d59d810f72e71cb87ab331b8
sHerrmann <khamarr3524@gmail.com>
2021-04-08 18:35:54 -0500

    Moved class TwitchBot
    TwitchBot class now has its own file. Still need to separate the GUI from the main file.

---
+++ t35-bot.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+"""
+Twitch Bot - Features to be Added
+    -Points System
+        Viewers accumulate points to be redeemed by the bot by:
+            - Being in Chat
+                How to check for users in chat? How often to check?
+                How many points/min? Do followers get a bonus? Subs (eventually? :])
+                    -Not following: 50 points / 6 minutes => 500 points / hour
+                    -Following: 150 points / 6 minutes => 1500 points / hour
+            - Following the Channel
+                One time Payout - Retroactive listing.
+                    If you're already following, but didn't get points, we need to track that.
+                    How?
+                        Does follow -> unfollow -> (Wait) -> follow count again??
+
+            - Mini-games
+"""
+
+
+import threading
+import sys
+from twitchbot import TwitchBot
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QWidget
+
+tbThread = 0
+
+
+def btn_test():
+    print("Test")
+
+
+def btn_exit():
+    tbThread.stop_task()
+    print("Exiting bot thread...")
+    print("Exiting...")
+    sys.exit()
+
+
+class BotThread(threading.Thread):
+    def __init__(self, target):
+        threading.Thread.__init__(self)
+        self.target = target
+
+    def run(self):
+        # noinspection PyUnresolvedReferences
+        self.target.start()
+
+    def stop_task(self):
+        # noinspection PyUnresolvedReferences
+        self.target.die()
+
+
+class T35GUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Twitch Bot - T35')
+        self.setFixedSize(250, 250)
+        self._centralWidget = QWidget(self)
+        self.setCentralWidget(self._centralWidget)
+        self._create_buttons()
+
+    def _create_buttons(self):
+        self.buttons = {
+            "exit": QPushButton("Exit")
+        }
+        self.buttons["exit"].setFixedSize(100, 40)
+        self.buttons["exit"].move(75, 105)
+        self.buttons["exit"].setParent(self)
+        self.buttons["exit"].clicked.connect(btn_exit)
+
+
+# noinspection PyTypeChecker
+def main():
+    args = []
+    f = open('args.txt', 'r')
+    for line in f:
+        args.append(line.rstrip())
+    global tbThread
+    tb = TwitchBot(args[0], args[1], args[2], args[3])
+    tbThread = BotThread(tb)
+    tbThread.start()
+
+    tbgui = QApplication([])
+    view = T35GUI()
+    view.show()
+    sys.exit(tbgui.exec_())
+
+
+if __name__ == "__main__":
+    main()
--- twitchbot.py
+++ twitchbot.py
@@ -1,35 +1,8 @@
-#!/usr/bin/env python3
-
-'''
-Twitch Bot - Features to be Added
-    -Points System
-        Viewers accumulate points to be redeemed by the bot by:
-            - Being in Chat
-                How to check for users in chat? How often to check?
-                How many points/min? Do followers get a bonus? Subs (eventually? :])
-                    -Not following: 50 points / 6 minutes => 500 points / hour
-                    -Following: 150 points / 6 minutes => 1500 points / hour
-            - Following the Channel
-                One time Payout - Retroactive listing.
-                    If you're already following, but didn't get points, we need to track that.
-                    How?
-                        Does follow -> unfollow -> (Wait) -> follow count again??
-
-            - Mini-games
-
-'''
 import constants
 import time
 import irc.bot
 import requests
-import threading
-import sys
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QWidget
 
-tbThread = 0
 validCommands = [
     "game",
     "title",
@@ -136,73 +109,3 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
 def _parse_nickname_from_twitch_user_id(user_id):
     # nickname!username@nickname.tmi.twitch.tv
     return user_id.split('!', 1)[0]
-
-
-def cmd_test():
-    print("Test")
-
-
-def cmd_exit():
-    print("Attempting to exit bot thread.")
-    tbThread.stop_task()
-    print("Bot thread exited.")
-
-
-class BotThread(threading.Thread):
-    def __init__(self, target):
-        threading.Thread.__init__(self)
-        self.target = target
-
-    def run(self):
-        # noinspection PyUnresolvedReferences
-        self.target.start()
-        print("Exiting BotThread...")
-
-    def stop_task(self):
-        # noinspection PyUnresolvedReferences
-        self.target.die()
-
-
-def _exit_click():
-    tbThread.stop_task()
-    sys.exit()
-
-
-class TwitchBotGUI(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle('Twitch Bot - T35')
-        self.setFixedSize(250, 250)
-        self._centralWidget = QWidget(self)
-        self.setCentralWidget(self._centralWidget)
-        self._create_buttons()
-
-    def _create_buttons(self):
-        self.buttons = {
-            "exit": QPushButton("Exit")
-        }
-        self.buttons["exit"].setFixedSize(100, 40)
-        self.buttons["exit"].move(75, 105)
-        self.buttons["exit"].setParent(self)
-        self.buttons["exit"].clicked.connect(_exit_click)
-
-
-# noinspection PyTypeChecker
-def main():
-    args = []
-    f = open('args.txt', 'r')
-    for line in f:
-        args.append(line.rstrip())
-    global tbThread
-    tb = TwitchBot(args[0], args[1], args[2], args[3])
-    tbThread = BotThread(tb)
-    tbThread.start()
-
-    tbgui = QApplication([])
-    view = TwitchBotGUI()
-    view.show()
-    sys.exit(tbgui.exec_())
-
-
-if __name__ == "__main__":
-    main()
